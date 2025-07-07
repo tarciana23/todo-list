86cebb4226c708f911e0575c55af727f4810d52c
Tarciana Oliveira <anarumiroan@gmail.com>
2025-07-07 10:56:36 -0300

    feat: criação da estrutura do projeto

---
+++ src/test/java/br/com/tarcianaoliveira/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tarcianaoliveira.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
